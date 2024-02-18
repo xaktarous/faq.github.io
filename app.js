2ba0892e1e842ae22d1f6eebbb1841a114c210b3
xaktarous <delali.abdallah@gmail.com>
2024-02-18 18:03:00 +0100

    change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const button=Array.from(document.querySelectorAll("#btn"));
 const button1=Array.from(document.querySelectorAll("#btn1"));
 const qst=Array.from(document.querySelectorAll("#hide-show"));
-
+const p=Array.from(document.querySelectorAll("#qst p"));
 
 for(let i=0;i<button.length;i++){
     button[i].addEventListener("click",()=>{
@@ -19,5 +19,18 @@ for(let i=0;i<button.length;i++){
       })
 }
 
+for(let i=0;i<p.length;i++){
+  p[i].addEventListener("click",()=>{
+    if(qst[i].style.display==="none" || qst[i].style.display===""){
+      button[i].style.display="none";
+      button1[i].style.display="block";
+      qst[i].style.display="block";
+    }else{
+      button1[i].style.display="none";
+      button[i].style.display="block";
+      qst[i].style.display="none";
+    }
 
+  })
+}
 
